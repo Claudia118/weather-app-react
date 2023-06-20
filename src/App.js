709d1154e9114ed5f7a84c9b4e47c0324abff205
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-06-20 13:54:38 -0600

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">
-    Weather App
-    </div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Claudia and is{" "}
+        <a
+          href="https://github.com/Claudia118/weather-app-react"
+          target="black"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
