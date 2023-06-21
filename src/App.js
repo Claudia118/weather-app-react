c90ecf9119543aedc99f6969634cabee1bf0b5ed
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-06-21 08:41:04 -0600

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Claudia and is{" "}
-        <a
-          href="https://github.com/Claudia118/weather-app-react"
-          target="black"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Claudia and is{" "}
+          <a
+            href="https://github.com/Claudia118/weather-app-react"
+            target="black"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
