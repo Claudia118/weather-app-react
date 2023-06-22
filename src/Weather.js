91dadc58351e1e6bf3445a13778221cd700a8bed
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-06-22 14:20:11 -0600

    Added WeatherInfo component

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -42,33 +42,7 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>
-            <FormattedDate date={weatherData.date} />
-          </li>
-          <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row ">
-          <div className="col-6">
-            <img
-              src={weatherData.icon}
-              alt={weatherData.description}
-              className="icon"
-            />
-            <span className="temperature">
-              {Math.round(weatherData.temperature)}
-            </span>
-            <span className="unit">°C</span>
-          </div>
-          <div className="col-6">
-            <ul className="city-description">
-              <li>Humidity: {weatherData.humidity}%</li>
-              <li>Wind: {weatherData.wind}</li>
-            </ul>
-          </div>
-        </div>
+        <WeatherInfo data= {weatherData}/>
       </div>
     );
   } else {
