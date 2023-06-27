fb0eca5733b54b80d2a1ae1bf2be162f418d0ae1
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-06-26 18:03:12 -0600

    Edit App

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-
-        <Weather defaultCity="Albuquerque"/>
+        <Weather defaultCity="Albuquerque" />
 
         <footer>
           This project was coded by Claudia and is{" "}
           <a
             href="https://github.com/Claudia118/weather-app-react"
-            target="black"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             open-sourced on GitHub
           </a>
