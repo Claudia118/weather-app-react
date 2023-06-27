67d4d3f08f8ca4055bc91e4fd1ff17e5098e8385
Claudia Ortiz <102268001+Claudia118@users.noreply.github.com>
2023-06-26 18:26:00 -0600

    removing imports that are not used

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,7 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemp from "./WeatherTemp";
-import WeatherForecast from "./WeatherForcast";
 
 export default function WeatherInfo(props) {
   return (
